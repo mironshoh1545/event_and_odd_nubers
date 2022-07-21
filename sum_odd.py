ac98dfb0755d28cdd898d4f77f5804b78fb5b782
Mironshoh <mironshoh1545@gmail.com>
2022-07-21 20:52:18 +0500

    ooo

--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,6 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+
+var_int = 4444
+print((var_int) % 2 * var_int % 10 + (var_int // 10) % 2 * var_int // 10 % 10 + (var_int // 100) % 2 * var_int // 100 % 10 + (var_int // 1000) % 2 * var_int // 1000 % 10)
